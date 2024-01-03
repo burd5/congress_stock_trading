bb86788eb74dbf00c56d39334ce8cf267d3679d0
burd5 <austinburdette5@gmail.com>
2024-01-03 10:11:57 -0700

    changed file structure

--- console.py
+++
@@ -1,4 +0,0 @@
-from data.models.scrape_house_stocks import initialize_webscrape, transaction_reports
-
-initialize_webscrape()
-print(transaction_reports)
--- settings.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DATABASE=os.getenv('DATABASE')
-USER=os.getenv('USER')
