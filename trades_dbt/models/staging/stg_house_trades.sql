f70ca7c0ff4556f2c8239343547728d22b42dfc2
burd5 <austinburdette5@gmail.com>
2024-03-25 18:01:19 -0600

    coerced amount, owner, transaction_date columns in staging for house_trades

--- trades_dbt/models/staging/stg_house_trades.sql
+++ trades_dbt/models/staging/stg_house_trades.sql
@@ -5,6 +5,7 @@ with house_trades as (
 remove_title as (
     select 
            id,
+           owner,
            CASE WHEN politician_name LIKE '%, Mr.. %' THEN 
                 split_part(politician_name,  ', Mr.. ', 2) || ' ' || split_part(politician_name,  ', Mr.. ', 1)
                 WHEN politician_name LIKE '%, Ms.. %' THEN 
@@ -22,17 +23,55 @@ remove_title as (
 
 first_and_last_only as (
     select id,
-           split_part(politician_name, ' ', 1) || ' ' || split_part(politician_name, ' ', -1) as politician_name
+            CASE 
+                WHEN SPLIT_PART(politician_name, ' ', 1) LIKE '%_.%' THEN SPLIT_PART(politician_name, ' ', 2) || ' ' || SPLIT_PART(politician_name, ' ', 3)
+                ELSE split_part(politician_name, ' ', 1) || ' ' || split_part(politician_name, ' ', -1)
+             END AS edited_politician_name
+           
     from remove_title
 ),
 
+edited_transaction_dates as (
+    select id,
+           SUBSTRING(transaction_date FROM '^\d{1,2}/\d{1,2}/\d{4}') as transaction_date
+    from house_trades
+),
+
+edited_amounts as (
+    select id,
+           CASE 
+                WHEN SPLIT_PART(amount, E'\n', 1) LIKE '%DC%' THEN SPLIT_PART(amount, E'\n', 2)
+                WHEN SPLIT_PART(amount, E'\n', 1) IN ('$25,000,001 -', '$1,000,001 -', '$5,000,001 -') THEN SPLIT_PART(amount, E'\n', 1) || ' ' || SPLIT_PART(amount, E'\n', 2)
+            ELSE
+                SPLIT_PART(amount, E'\n', 1)
+    END AS amount
+    from house_trades
+),
+
 modify_purchased_or_sold as (
-    select CASE WHEN purchased_or_sold = 'S' THEN 'Sale'
+    select id,
+           CASE WHEN purchased_or_sold = 'S' THEN 'Sale'
                 WHEN purchased_or_sold = 'P' THEN 'Purchase'
                 WHEN purchased_or_sold = 'E' THEN 'Exchange'
                 WHEN purchased_or_sold = 'S (partial)' THEN 'Sale (partial)'
                 END as purchased_or_sold
     from house_trades
+),
+
+combine_edited_columns as (
+    select 
+          mod.id as id,
+          UPPER(owner) as owner,
+          fl.edited_politician_name as politician_name,
+          stock_information,
+          mod.purchased_or_sold as purchased_or_sold,
+          etd.transaction_date as transaction_date,
+          ea.amount
+    from first_and_last_only fl join modify_purchased_or_sold mod
+              on fl.id = mod.id join house_trades 
+              on house_trades.id = mod.id join edited_transaction_dates etd
+              on mod.id = etd.id join edited_amounts ea 
+              on etd.id = ea.id
 )
 
-select * from first_and_last_only
+select * from combine_edited_columns
