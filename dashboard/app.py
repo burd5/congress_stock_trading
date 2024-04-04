2ec57e92df998618f1edf6784993d9e3edc19130
burd5 <austinburdette5@gmail.com>
2024-04-04 09:07:54 -0600

    created frontend folder, edited DBT house trades file, and deleted streamlit app

--- dashboard/app.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-import awswrangler as wr
-
--- trades_dbt/models/staging/stg_house_trades.sql
+++ trades_dbt/models/staging/stg_house_trades.sql
@@ -5,7 +5,6 @@ with house_trades as (
 remove_title as (
     select 
            id,
-           owner,
            CASE WHEN politician_name LIKE '%, Mr.. %' THEN 
                 split_part(politician_name,  ', Mr.. ', 2) || ' ' || split_part(politician_name,  ', Mr.. ', 1)
                 WHEN politician_name LIKE '%, Ms.. %' THEN 
@@ -17,7 +16,11 @@ remove_title as (
                 WHEN politician_name LIKE '%,%' THEN 
                 split_part(politician_name,  ', ', 2) || ' ' || split_part(politician_name,  ', ', 1)  
           ELSE politician_name
-          END as politician_name
+          END as politician_name,
+          stock_information,
+          purchased_or_sold,
+          transaction_date,
+          amount
     from house_trades
 ),
 
@@ -26,66 +29,79 @@ first_and_last_only as (
             CASE 
                 WHEN SPLIT_PART(politician_name, ' ', 1) LIKE '%_.%' THEN SPLIT_PART(politician_name, ' ', 2) || ' ' || SPLIT_PART(politician_name, ' ', 3)
                 ELSE split_part(politician_name, ' ', 1) || ' ' || split_part(politician_name, ' ', -1)
-             END AS edited_politician_name
-           
+             END AS edited_politician_name,
+             stock_information,
+            purchased_or_sold,
+            transaction_date,
+            amount
     from remove_title
 ),
 
 edited_transaction_dates as (
     select id,
-           SUBSTRING(transaction_date FROM '^\d{1,2}/\d{1,2}/\d{4}') as transaction_date
-    from house_trades
+          edited_politician_name as politician_name,
+          stock_information,
+          purchased_or_sold,
+          SUBSTRING(transaction_date FROM '^\d{1,2}/\d{1,2}/\d{4}') as transaction_date,
+          amount
+    from first_and_last_only
 ),
 
 edited_amounts as (
     select id,
+           politician_name,
+           stock_information,
+           purchased_or_sold,
+           transaction_date,
            CASE 
                 WHEN SPLIT_PART(amount, E'\n', 1) LIKE '%DC%' THEN SPLIT_PART(amount, E'\n', 2)
                 WHEN SPLIT_PART(amount, E'\n', 1) IN ('$25,000,001 -', '$1,000,001 -', '$5,000,001 -') THEN SPLIT_PART(amount, E'\n', 1) || ' ' || SPLIT_PART(amount, E'\n', 2)
             ELSE
                 SPLIT_PART(amount, E'\n', 1)
     END AS amount
-    from house_trades
+    from edited_transaction_dates
 ),
 
 split_on_filing as (
     select id,
-           SPLIT_PART(UPPER(stock_information), 'FILING', 1) as stock_information
-    from house_trades
+           politician_name,
+           SPLIT_PART(UPPER(stock_information), 'FILING', 1) as stock_information,
+           purchased_or_sold,
+           transaction_date,
+           amount
+    from edited_amounts
 ),
 
 edited_stock_information as (
     select id,
-           (REGEXP_MATCHES(stock_information, '\(([a-zA-Z]+)\)'))[1] as stock_ticker,
-           SPLIT_PART(stock_information, E'\n', 1) || ' ' || SPLIT_PART(stock_information, E'\n', 2) as stock_information
+           politician_name,
+        --    (REGEXP_MATCHES(stock_information, '\(([a-zA-Z]+)\)'))[1] as stock_ticker,
+           CASE 
+               WHEN POSITION('(' IN stock_information) > 0 AND POSITION(')' IN stock_information) > 0 THEN
+                   SUBSTRING(stock_information, POSITION('(' IN stock_information) + 1, POSITION(')' IN stock_information) - POSITION('(' IN stock_information) - 1)
+               ELSE '--'  -- Or any other default value you prefer when parentheses are not found
+           END as stock_ticker,
+           SPLIT_PART(stock_information, E'\n', 1) || ' ' || SPLIT_PART(stock_information, E'\n', 2) as stock_information,
+           purchased_or_sold,
+           transaction_date,
+           amount
     from split_on_filing
 ),
 
 modify_purchased_or_sold as (
     select id,
+           politician_name,
+           stock_ticker,
+           stock_information,
            CASE WHEN purchased_or_sold = 'S' THEN 'Sale'
                 WHEN purchased_or_sold = 'P' THEN 'Purchase'
                 WHEN purchased_or_sold = 'E' THEN 'Exchange'
                 WHEN purchased_or_sold = 'S (partial)' THEN 'Sale (partial)'
-                END as purchased_or_sold
-    from house_trades
-),
-
-combine_edited_columns as (
-    select 
-          mod.id as id,
-          fl.edited_politician_name as politician_name,
-          es.stock_ticker as stock_ticker,
-          es.stock_information,
-          mod.purchased_or_sold as purchased_or_sold,
-          etd.transaction_date as transaction_date,
-          ea.amount
-    from first_and_last_only fl left join modify_purchased_or_sold mod
-              on fl.id = mod.id left join house_trades 
-              on house_trades.id = mod.id left join edited_transaction_dates etd
-              on mod.id = etd.id left join edited_amounts ea 
-              on etd.id = ea.id left join edited_stock_information es 
-              on ea.id = es.id
+                ELSE purchased_or_sold
+                END as purchased_or_sold,
+            transaction_date,
+            amount
+    from edited_stock_information
 )
 
-select * from combine_edited_columns
+select * from modify_purchased_or_sold
